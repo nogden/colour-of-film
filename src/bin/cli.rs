3c8e595dc7e7842052ea97d36632be636717fcf2
Nick Ogden <nick@nickogden.org>
2021-01-31 18:24:42 +0000

    Support creating images from key frames only

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,12 +1,12 @@
 use std::{error::Error, env};
 
-use colour_of_film::stripe_image;
+use colour_of_film::{stripe_image, Mode};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args_os();
     let _ = args.next(); // Skip executable name
     if let Some(path) = args.next() {
-        let image = stripe_image(path, 400)?;
+        let image = stripe_image(path, 100, Mode::KeyFramesOnly)?;
         image.save("output.png")?;
         Ok(())
     } else {
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,19 @@ use ffav::{
     media,
     software::{self, scaling::flag::Flags},
     util::frame::video::Video,
+    Packet, Stream,
 };
-use image::{RgbImage, Rgb};
+use image::{imageops, RgbImage};
 use std::{error::Error, path::Path, convert::TryInto, io::Write, time::Instant};
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum Mode {
+    AllFrames,
+    KeyFramesOnly,
+}
+
 pub fn stripe_image<P: AsRef<Path>>(
-    path: P, image_height: u32,
+    path: P, image_height: u32, mode: Mode
 ) -> Result<RgbImage, Box<dyn Error>> {
     ffav::init()?;
 
@@ -17,7 +24,6 @@ pub fn stripe_image<P: AsRef<Path>>(
         .best(media::Type::Video)
         .ok_or("Couldn't find video stream".to_owned())?;
     let video_stream_index = video_stream.index();
-    let frame_count: u32 = video_stream.frames().try_into()?;
     let mut decoder = video_stream.codec().decoder().video()?;
     let mut scale_and_convert = software::scaling::Context::get(
         decoder.format(),
@@ -30,13 +36,33 @@ pub fn stripe_image<P: AsRef<Path>>(
     )?;
     let mut frame_index = 0;
     let mut frame = Video::empty();
-    let mut image = RgbImage::new(frame_count, image_height);
+
+    let frame_count: u32;
+    let mut all_frames_iter;
+    let mut key_frames_iter;
+    let packets: &mut dyn Iterator<Item=(Stream, Packet)>;
+    packets = if mode == Mode::KeyFramesOnly {
+        frame_count = 277; /*video_file.packets().filter(|(stream, packet)| {
+            stream.index() == video_stream_index  && packet.is_key()
+        }).count() as u32;*/
+        key_frames_iter = video_file.packets().filter(|(stream, packet)| {
+            stream.index() == video_stream_index && packet.is_key()
+        });
+        &mut key_frames_iter
+    } else {
+        frame_count = video_stream.frames().try_into()?;
+        all_frames_iter = video_file.packets().filter(|(stream, _)| {
+            stream.index() == video_stream_index
+        });
+        &mut all_frames_iter
+    };
+
+    let mut image_buffer = Vec::with_capacity(
+        (frame_count * image_height * 3) as usize
+    );
 
     let start_time = Instant::now();
-    for (stream, packet) in video_file.packets() {
-        if stream.index() != video_stream_index {
-            continue;
-        }
+    for (_, packet) in packets {
         // Shove in the packet
         if decoder.decode(&packet, &mut frame)? {
             // A frame popped out; scale it
@@ -48,15 +74,15 @@ pub fn stripe_image<P: AsRef<Path>>(
                 bytes.get_unchecked(1),
                 bytes.get_unchecked(2),
             )};
-            for line in 0..image_height {
-                image.put_pixel(frame_index, line, Rgb([*r, *g, *b]));
+            for _ in 0..image_height {
+                image_buffer.extend_from_slice(&[*r, *g, *b]);
             }
             frame_index += 1;
-            if frame_index % 500 == 0 {
+            if frame_index % 200 == 0 {
                 let rate = frame_index as f32 / start_time.elapsed().as_secs_f32();
                 let eta = (frame_count - frame_index) as f32 / rate;
                 print!(
-                    "\rProgress: {:>3}%\t{:.2}fps\tETA: {}m {}s\t\t",
+                    "\rProgress: {:>3}%    {:.2}fps    ETA: {}m {}s    ",
                     ((frame_index as f32 / frame_count as f32) * 100.) as u32,
                     rate,
                     (eta / 60.0) as u32,
@@ -66,6 +92,9 @@ pub fn stripe_image<P: AsRef<Path>>(
             }
         }
     }
+    println!();
+    let image = RgbImage::from_vec(image_height, frame_index, image_buffer)
+        .ok_or("Image buffer wasn't big enough".to_owned())?;
 
-    Ok(image)
+    Ok(imageops::rotate90(&image))
 }
