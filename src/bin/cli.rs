4a710b1ce4bfd00c95aa761dd0b997bd559373c9
Nick Ogden <nick@nickogden.org>
2021-02-04 18:49:00 +0000

    Allow choosing the operating mode via a command line flag

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,15 +1,31 @@
-use std::{error::Error, env};
+use std::{error::Error, ffi::OsString};
 
+use clap::Clap;
 use colour_of_film::{stripe_image, Mode};
 
+/// Create a colour profile of a video by taking the average colour of
+/// each frame
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Nick Ogden <nick@nickogden.org>")]
+struct Options {
+    /// Path to the input video file
+    #[clap(index(1))]
+    video_file: OsString,
+
+    /// Use every frame of the input video instead of just key frames
+    /// (very slow for long videos)
+    #[clap(short, long)]
+    all_frames: bool,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut args = env::args_os();
-    let _ = args.next(); // Skip executable name
-    if let Some(path) = args.next() {
-        let image = stripe_image(path, 100, Mode::KeyFramesOnly)?;
-        image.save("output.png")?;
-        Ok(())
+    let options = Options::parse();
+    let mode = if options.all_frames {
+        Mode::AllFrames
     } else {
-        Err("A film file must be provided".into())
-    }
+        Mode::KeyFramesOnly
+    };
+    let image = stripe_image(options.video_file, 100, mode)?;
+    image.save("output.png")?;
+    Ok(())
 }
